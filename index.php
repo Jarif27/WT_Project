a9518e6704161f88ac5a16a2ec7fb1272bc08637
Jarif27 <jarifislam01712@gmail.com>
2026-05-18 15:31:59 +0600

    jarif

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 /*
 Demo login.
 Task 1 login system merge করার আগে test করার জন্য।
-Buyer Sarah = user id 3.ssadasdasdas
+Buyer Sarah = user id 3.ss
 */
 if (!isset($_SESSION['user_id'])) {
     $_SESSION['user_id'] = 3;
